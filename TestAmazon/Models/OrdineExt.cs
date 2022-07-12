c1859cc912b661d26a886acbc599d5af6d0d5e75
EmanueleRotatori <e.rotatori-ext@stepsrl.org>
2022-07-12 16:57:28 +0200

    Partial carrello e ordine

---
+++ TestAmazon/Models/OrdineExt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestAmazon.Models
+{
+    public partial class Ordine
+    {
+        public static void AddOrdine(int idUtente)
+        {
+            Ordine ord = new Ordine();
+            ord.Acquistato = false;
+
+            using(var db = new CorsoRoma2022Entities())
+            {
+                ord.Id_Utente = idUtente;
+                db.Ordine.Add(ord);
+                db.SaveChanges();
+            }
+        }
+
+        public static void SetOrdine(int idOrdine)
+        {
+            using(var db = new CorsoRoma2022Entities())
+            {
+                Ordine ord = db.Ordine.SingleOrDefault(o => o.Id_Ordine == idOrdine);
+                if (ord != null)
+                {
+                    ord.Acquistato = true;
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
