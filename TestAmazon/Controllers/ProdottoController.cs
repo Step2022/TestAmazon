cf96af3fd871db1a186c42340ff9f6e248161ff0
User01 <User01@DESKTOP-4Q2569S>
2022-07-12 16:55:58 +0200

    Aggiunta ProdottoController

---
+++ TestAmazon/Controllers/ProdottoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestAmazon.Controllers
+{
+    public class ProdottoController : Controller
+    {
+        // GET: Prodotto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Singolo()
+        {
+            ViewBag.Message = "Pagina del prodotto singolo.";
+
+            return View();
+        }
+
+    }
+}
