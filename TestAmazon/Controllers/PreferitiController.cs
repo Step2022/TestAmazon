fb4655d72d2e8dfdad6b9dbc56dedce20825e431
Matteo Napoli <m.napoli-ext@stepsrl.org>
2022-07-13 17:42:27 +0200

    Fix Progetto

--- TestAmazon/Controllers/PreferitiController.cs
+++ TestAmazon/Controllers/PreferitiController.cs
@@ -1,17 +1,66 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Web;
+
 using System.Web.Mvc;
 
+using TestAmazon.Models;
+
+
+
 namespace TestAmazon.Controllers
+
 {
+
     public class PreferitiController : Controller
+
     {
-        // GET: Preferiti
+
+
+
         public ActionResult Index()
+
         {
+
             return View();
+
         }
+
+        public ActionResult Preferiti(long id)
+
+        {
+
+
+
+            return View(PreferitiPartial.GetPreferiti(id));
+
+        }
+
+
+
+        public ActionResult Remove(long id_utente, long id_prodotto)
+
+        {
+
+
+
+
+            string C = PreferitiPartial.RemovePreferiti(id_utente, id_prodotto);
+
+
+
+            return View("Preferiti");
+
+
+
+        }
+
+
+
     }
+
 }
