e1aba3a3c62e6eecd5310164a585b8debccfca90
Alessandro Tedeschi <alessandro.tedeschi@stepsrl.org>
2022-07-13 16:27:14 +0200

    ag

---
+++ TestAmazon/Models/CategoriaPartial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestAmazon.Models
+{
+    public partial class Categoria
+    {
+        public static List<Categoria> GetCategorie()
+        {
+            List<Categoria> categorie = new List<Categoria>();
+            using(CorsoRoma2022Entities db = new CorsoRoma2022Entities())
+            {
+                categorie.AddRange(db.Categoria);
+            }
+            return categorie;
+        }
+    }
+}
