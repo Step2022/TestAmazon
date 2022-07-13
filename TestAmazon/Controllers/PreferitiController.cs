4727bfb8cfaac0d31baa9271346c790357af1d6c
user <user@DESKTOP-VC3RPF4>
2022-07-13 10:18:41 +0200

    controller preferiti

---
+++ TestAmazon/Controllers/PreferitiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestAmazon.Controllers
+{
+    public class PreferitiController : Controller
+    {
+        // GET: Preferiti
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
