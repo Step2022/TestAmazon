9446fbb2c52466a48b184d2410c601343f0fed91
EmanueleRotatori <e.rotatori-ext@stepsrl.org>
2022-07-14 12:21:35 +0200

    commit carrelloUtente view

--- TestAmazon/Controllers/CarrelloController.cs
+++ TestAmazon/Controllers/CarrelloController.cs
@@ -11,9 +11,8 @@ namespace TestAmazon.Controllers
     {
         public ActionResult CarrelloUtente()
         {
-            //Utente utente = (Utente)Session["utente"];
-            long idOrdine = Ordine.GetIdOrdine(1);
-            ViewBag.idOrdine = idOrdine;
+            long idOrdine = Ordine.GetIdOrdine(1/*(long)Session["IdUtente"]*/);
+            ViewBag.preferiti = PreferitiPartial.GetPreferiti(1/*(long)Session["IdUtente"]*/);
             return View(Carrello.GetCarrello(idOrdine));
         }
     }
