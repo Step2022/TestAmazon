1d4d55e7cb4a37c109a7418ea0aad2c0dc2c3f83
Matteo Napoli <m.napoli-ext@stepsrl.org>
2022-07-16 17:15:29 +0200

    Impaginazione

--- TestAmazon/Scripts/js/homepage.js
+++ TestAmazon/Scripts/js/homepage.js
@@ -17,7 +17,7 @@ function onMouseLeaveImage(id) {
 function OnChangeDDLUser(ids) {
     let select = document.getElementById("select_user");
     switch (select.value) {
-        case 0:
+        case "0":
 
             break;
         case "1":
@@ -26,14 +26,114 @@ function OnChangeDDLUser(ids) {
                 $.ajax({
                     type: 'GET',
                     success: function (data) {
-                        window.location.href = "~/../../Preferiti/Preferiti/"+ids;
+                        window.location.href = "../../Preferiti/Preferiti/"+ids;
                     },
                     error: function () {
                         alert('error happened');
                     }
-                    });
-                }
+                });
+            }
 
-        break;
+            break;
+        case "2":
+            if (ids != null) {
+                $.ajax({
+                    type: 'GET',
+                    success: function (data) {
+                        window.location.href = "../../Carrello/CarrelloUtente";
+                    },
+                    error: function () {
+                        alert('error happened');
+                    }
+                });
+            }
+            break;
+    }
+}
+function plus(NumeroPagine) {
+    let label = document.getElementById("numPag");
+    if (label != null) {       
+        let stringa = "../../Home/Index?pag=";
+        if ((new RegExp("^[0-9]+$")).test(label.innerHTML.trim()) ) {
+            stringa += (parseInt(label.innerHTML.trim())+1);
+
+        } else {
+            stringa += 1;
+        }
+        if (parseInt(label.innerHTML.trim()) < NumeroPagine) {
+            $.ajax({
+                type: 'GET',
+                success: function (data) {
+                    window.location.href = stringa;
+                },
+                error: function () {
+                    alert('error happened');
+                }
+            });
+        }
+    }
+}
+function minus() {
+    let label = document.getElementById("numPag");
+    if (label != null) {
+        if ((new RegExp("^[0-9]+$")).test(label.innerHTML.trim())) {
+            if (parseInt(label.innerHTML.trim()) > 1) {
+                let stringa = "../../Home/Index?pag=";
+                if ((new RegExp("^[0-9]+$")).test(label.innerHTML.trim())) {
+                    stringa += (parseInt(label.innerHTML.trim())-1);
+
+                } else {
+                    stringa += 1;
+                }
+                $.ajax({
+                    type: 'GET',
+                    success: function (data) {
+                        window.location.href = stringa;
+                    },
+                    error: function () {
+                        alert('error happened');
+                    }
+                });
+            }
+        } else {
+            label.innerHTML = 0
+        }
+    }
+}
+function first() {
+    let label = document.getElementById("numPag");
+    if (label != null) {
+        if ((new RegExp("^[0-9]+$")).test(label.innerHTML.trim())) {
+            if (parseInt(label.innerHTML.trim()) != 0) {                
+                $.ajax({
+                    type: 'GET',
+                    success: function (data) {
+                        window.location.href = "../../Home/Index?pag=1";
+                    },
+                    error: function () {
+                        alert('error happened');
+                    }
+                });
+            }
+        }
+
+    }
+}
+function last(NumeroPagine) {
+    let label = document.getElementById("numPag");
+    if (label != null) {
+        if ((new RegExp("^[0-9]+$")).test(label.innerHTML.trim())) {
+            if (label.innerHTML.trim() != NumeroPagine) {
+                $.ajax({
+                    type: 'GET',
+                    success: function (data) {
+                        window.location.href = "../../Home/Index?pag=" + NumeroPagine;
+                    },
+                    error: function () {
+                        alert('error happened');
+                    }
+                });
+            }
+        }
     }
 }
